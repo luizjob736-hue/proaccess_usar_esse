0f580d5085c31286666c2b01ccd4eb888a1b0ca9
luizjob736-hue <luizjob736@gmail.com>
2026-08-19 15:08:42 -0300

    feat: import cn utility in importar route
    Add utility function import to support dynamic class name merging.

--- src/routes/_authenticated/importar.tsx
+++ src/routes/_authenticated/importar.tsx
@@ -6,6 +6,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   Upload,
